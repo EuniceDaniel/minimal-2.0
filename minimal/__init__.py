3ab12cdfc7e8d35fc6f9730726db7dd93a70219e
EuniceDaniel <141869912+EuniceDaniel@users.noreply.github.com>
2023-11-22 14:58:59 -0600

    first commit

---
+++ minimal/__init__.py
@@ -0,0 +1,12 @@
+def my_sum(x, y):
+    """A function that sums. """
+    return x+y
+
+
+def my_mul(x, y):
+    """A function that multiply. """
+    return x*y
+
+
+def my_rest(x, y):
+    return x-y
